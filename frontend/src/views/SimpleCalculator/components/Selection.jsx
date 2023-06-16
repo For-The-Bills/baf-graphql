8b304b0d9994adaa2a459829fd4b970c902b5cf8
demon <naimadokwas@gmail.com>
2023-06-16 20:20:01 +0200

    Selection calculator

--- frontend/src/views/SimpleCalculator/components/Selection.jsx
+++ frontend/src/views/SimpleCalculator/components/Selection.jsx
@@ -1,61 +1,69 @@
 import React, { useState } from "react"
-import { FormControl, FormControlLabel, Radio, RadioGroup } from "@mui/material"
 import styles from "./Selection.module.scss"
+
+import { InputLabel, MenuItem, Select } from "@mui/material"
+import FormControl from "@mui/material/FormControl"
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt"
 
 function Selection(props) {
   const [selectedType, setSelectedType] = useState("")
+  const [indicatorValue, setIndicatorValue] = useState(
+    "Wybierz rodzaj zabudowy"
+  )
+
+  const indicatorDictionary = {
+    mieszkaniowa: 0.6,
+    "przestrzenie publiczne": 0.6,
+    usługowa: 0.3,
+    produkcyjna: 0.3,
+    "usługowo-produkcyjna": 0.3,
+    "usługowo-mieszkaniowa": 0.5,
+    "składy i magazyny": 0.3,
+  }
 
   const handleTypeChange = (event) => {
-    setSelectedType(event.target.value)
+    const selectedValue = event.target.value
+    setSelectedType(selectedValue)
+    setIndicatorValue(indicatorDictionary[selectedValue] || "błąd")
   }
 
   return (
     <div className={styles.selectionContainer}>
-      <FormControl component="fieldset">
-        <RadioGroup
-          aria-label="building-type"
-          name="building-type"
-          value={selectedType}
-          onChange={handleTypeChange}
-          row
-        >
-          <FormControlLabel
-            value="mieszkaniowa"
-            control={<Radio />}
-            label="Mieszkaniowa"
-          />
-          <FormControlLabel
-            value="przestrzenie publiczne"
-            control={<Radio />}
-            label="Przestrzenie publiczne"
-          />
-          <FormControlLabel
-            value="usługowa"
-            control={<Radio />}
-            label="Usługowa"
-          />
-          <FormControlLabel
-            value="produkcyjna"
-            control={<Radio />}
-            label="Produkcyjna"
-          />
-          <FormControlLabel
-            value="usługowo-produkcyjna"
-            control={<Radio />}
-            label="Usługowo-produkcyjna"
-          />
-          <FormControlLabel
-            value="usługowo-mieszkaniowa"
-            control={<Radio />}
-            label="Usługowo-mieszkaniowa"
-          />
-          <FormControlLabel
-            value="składy i magazyny"
-            control={<Radio />}
-            label="Składy i magazyny"
-          />
-        </RadioGroup>
-      </FormControl>
+      <div className={styles.typeOfBuildingContainer}>
+        <div className={styles.typeOfBuildingSelect}>
+          <FormControl sx={{ m: 1, minWidth: 220 }}>
+            <InputLabel id="building-type-label">Rodzaj zabudowy</InputLabel>
+            <Select
+              labelId="building-type-label"
+              id="building-type"
+              value={selectedType}
+              onChange={handleTypeChange}
+              label="Wybierz rodzaj zabudowy"
+            >
+              <MenuItem value="mieszkaniowa">Mieszkaniowa</MenuItem>
+              <MenuItem value="przestrzenie publiczne">
+                Przestrzenie publiczne
+              </MenuItem>
+              <MenuItem value="usługowa">Usługowa</MenuItem>
+              <MenuItem value="produkcyjna">Produkcyjna</MenuItem>
+              <MenuItem value="usługowo-produkcyjna">
+                Usługowo-produkcyjna
+              </MenuItem>
+              <MenuItem value="usługowo-mieszkaniowa">
+                Usługowo-mieszkaniowa
+              </MenuItem>
+              <MenuItem value="składy i magazyny">Składy i magazyny</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className={styles.arrow}>
+          <ArrowRightAltIcon></ArrowRightAltIcon>
+        </div>
+        <div className={styles.minimalBaf}>
+          <p>Minimalny wskaźnik baf:</p>
+          <p>{indicatorValue}</p>
+        </div>
+      </div>
     </div>
   )
 }

c57e99d63549e443e1197074518d74ac3f41a6de
demon <naimadokwas@gmail.com>
2023-06-17 18:58:11 +0200

    navbar logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
 import styles from "./Navbar.module.scss"
+import { matchPath, withRouter } from "react-router"
+import bafLogo from "../../assets/images/logo.png"
+import {
+  motion,
+  useAnimationControls,
+  useCycle,
+  useScroll,
+} from "framer-motion"
+import { useHistory } from "react-router-dom"
 
-function Navbar(props){
-    return(
-        <div className={styles.navbarContainer}>
-            Navbar
-        </div>
-    )
+function Navbar(props) {
+  const history = useHistory()
+
+  return (
+    <div className={styles.navbarContainer}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+      >
+        <img
+          onClick={() => {
+            history.push("/")
+          }}
+          className={styles.logo}
+          src={bafLogo}
+          alt="Dąbrowa Górnicza kalkulator BAF"
+        />
+      </motion.div>
+    </div>
+  )
 }
 
 export default Navbar
